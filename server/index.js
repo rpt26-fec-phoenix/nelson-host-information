8659ed88972cd2a35f7a60bda651033c73fbd995
vinayaachar <vinayaachar@gmail.com>
2021-06-05 22:35:52 -0700

    New Relic and Artillery config

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const db = require('../database/db.js');
-const PORT = 3007;
+const PORT = 3000;
 const cors = require('cors');
+const pool = require('../database/postgresDb.js');
+const newrelic = require('newrelic');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,6 +20,40 @@ app.get('/:listingID/host', (req, res) => {
     res.end();
   });
 });
+
+
+//Get route
+app.get('/hosts/:id', (req, res) => {
+  let request = `
+      SELECT * FROM hostinformation
+      WHERE id = ${req.params.id}
+  `;
+
+  pool.query(request, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).json(data.rows);
+  });
+});
+
+
+//Post route
+app.post('/addHosts', (req, res) => {
+
+  const insert = 'INSERT INTO hostinformation (host_name, date_joined, profile_pic, host_description, review_COUNT, is_verified, is_superhost, listing_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8)';
+
+  var params = req.body;
+
+  pool.query(insert, [params.host_name, params.date_joined, params.profile_pic, params.host_description, params.review_COUNT, params.is_verified, params.is_superhost, params.listing_id], (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).send('Users added');
+  });
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
