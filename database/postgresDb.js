e5ca750ad7063fc01e36fcc4e044e4b661ce35e5
vinayaachar <vinayaachar@gmail.com>
2021-06-14 20:43:07 -0700

    point to new db instance

--- database/postgresDb.js
+++ database/postgresDb.js
@@ -4,7 +4,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'postgres',
   password: 'qwerty',
-  host: 'localhost',
+  host: '172.31.29.239',
   port: 5432,
   database: 'airbnb'
 });
